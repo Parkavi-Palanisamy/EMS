5cd4c49a36c216bdbcfb02b79261f92e0a8d73ab
Parkavi-Palanisamy <palanikavi120705@gmail.com>
2026-03-14 13:34:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, psycopg2
 
 app = Flask(__name__)
 
@@ -8,3 +8,48 @@ def home():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+    conn = psycopg2.connect(
+        host="your-host",
+        database="postgres",
+        user="postgres",
+        password="your-password"
+    )
+
+
+    @app.route("/")
+    def home():
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM employees")
+        data = cur.fetchall()
+        return str(data)
+
+    app.run()
+
+    conn = psycopg2.connect(
+        host="YOUR_HOST",
+        database="postgres",
+        user="postgres",
+        password="YOUR_PASSWORD",
+        port="5432"
+    )
+
+
+    @app.route("/")
+    def home():
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM employees")
+        data = cur.fetchall()
+        cur.close()
+        return render_template("index.html", employees=data)
+
+
+    if __name__ == "__main__":
+        app.run(debug=True)
+
+from db import conn, cursor
+
+cursor.execute("SELECT * FROM users")
+data = cursor.fetchall()
+
+print(data)
